5d8fdec09b85d590b12daf724878d194b143164d
ginobili13 <gin0bili13god@gmail.com>
2017-05-30 18:47:27 +0200

    2. Constructores

--- tarea1GitGui/receta.java
+++ tarea1GitGui/receta.java
@@ -2,4 +2,20 @@ package tarea1GitGui;
 
 public class receta {
 
+	String nombre;
+	String ingredientes;
+	String preparacion;
+			
+	public receta(String nombre, String ingredientes, String preparacion) {
+		super();
+		this.nombre = nombre;
+		this.ingredientes = ingredientes;
+		this.preparacion = preparacion;
+	}
+	
+	public receta(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+	
 }
